543433c72a7111872d5096a020d535a3b58d2c61
chris <cbriant@btinternet.com>
2023-11-29 05:41:40 +0000

    clean-up

--- src/components/SocialPanel.js
+++ src/components/SocialPanel.js
@@ -1,21 +1,5 @@
 import { ReactComponent as Face1 } from '../assets/face_1_plain.svg';
 
-// const profile = {
-//   "id": 5,
-//   "name": "Fatuma",
-//   "profile_age": {
-//       "age": "70s"
-//   },
-//   "sex_and_race": {
-//       "sex": "Female",
-//       "race": "White"
-//   },
-//   "profession": "Engineer",
-//   "bio": "With several decades of expertise under my belt, I consider myself as a seasoned professional in the engineering field. I am in my 70s but I am unstoppable. Despite my age, I remain as an active and engaged protagonist in this ever-evolving industry. Over the decades, I've had the opportunity to steer major projects that have formulated the engineering landscape today. Even in the twilight years of my life, I continue to foster a burning desire to shape the coming generation of engineers and act as a guiding light for them to navigate through their careers.",
-//   "interests": "Outside the constraints of engineering blueprints and prototypes, I found solace in seemingly unusual hobbies. I devote a significant portion of my leisure time in stockers and ballies—two pastimes that, interestingly, tone my strategic thinking and provide a fun way to alleviate routine stress. Stockers feeds my fascination with market dynamics and investment strategies, while ballies, an exciting game, reignites the competitive streak in me, quintessential in my field of work. Amidst these, I enjoy absorbing new knowledge through reading and engaging in community service, subtly maintaining a touch of balance in life.",
-//   "photo_url": null,
-//   "user_name": "ever-evolving10"
-// };
 
 const SocialPanel = (props) => {
   const profile = props.profile;
